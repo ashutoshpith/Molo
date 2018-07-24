98be296c89d12557488f35f69c58a4b7a4358d17
ashutosh <amxdp18@gmail.com>
2018-07-25 00:43:37 +0530

    migration applied

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+     path('<int:ques_id>/', views.detail, name='detail'),
+     path('<int:ques_id>/results/', views.results, name='results'),
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from .models import Question
 # Create your views here.
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.ques_text for q in latest_question_list])
+    return HttpResponse(output)
+ 
+def detail(request, ques_id):
+    return HttpResponse("Hello, world. You're at the polls question %s." %ques_id)
+
+def results(request, ques_id):
+    response = 'You are looking at result %s'
+    return HttpResponse(response % ques_id)
